02279c60277b001adacfd4dca50d530d6bae2310
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-03-28 17:36:39 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
+// Updated by Lynn Lukose
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates area of a triangle.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculateAreaOfTriangle () {
+  // input
+  const baseOfTriangle = parseInt(document.getElementById('base-of-triangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const areaOfTriangle = (baseOfTriangle * heightOfTriangle) / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
 }
