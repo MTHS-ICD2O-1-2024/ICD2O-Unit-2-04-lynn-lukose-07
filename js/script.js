c6ceabc8da97d2e3706c1ffeef995f8a115cbea6
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-04-01 16:53:38 +0000

    edited js

--- js/script.js
+++ js/script.js
@@ -10,14 +10,19 @@
 /**
  * This function calculates area of a triangle.
  */
-function calculateAreaOfTriangle () {
+function calculateAreaOfTriangle() {
   // input
-  const baseOfTriangle = parseInt(document.getElementById('base-of-triangle').value)
-  const heightOfTriangle = parseInt(document.getElementById('height-of-triangle').value)
+  const baseOfTriangle = parseInt(
+    document.getElementById("base-of-triangle").value
+  )
+  const heightOfTriangle = parseInt(
+    document.getElementById("height-of-triangle").value
+  )
 
   // process
   const areaOfTriangle = (baseOfTriangle * heightOfTriangle) / 2
 
   // output
-  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
+  document.getElementById("area").innerHTML =
+    "Area is: " + areaOfTriangle + " cm²"
 }
